08f02e32adda987a48e922d41de08f2acd608eee
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-09-22 00:35:38 +0300

    done with the lab

--- server/app.py
+++ server/app.py
@@ -18,5 +18,22 @@ def print_string(parameter):
 
 @app.route("/count/<int:parameter>")
 def count(parameter):
-    int(parameter)
+    num = '\n'.join(str(i) for i in range (parameter))
+    return f"{num}\n"
 
+@app.route("/math/<init:num1>/<operation>/<int:num2>")
+def math(num1,operation,num2):
+    result = None
+    
+    if operation =='+':
+        result = num1 + num2
+    elif operation =='-':
+        result = num1 - num2
+    elif operation =='*':
+        result = num1 * num2
+    elif operation =='div':
+        result = num1 / num2
+    elif operation =='%':
+        result = num1 % num2
+        
+    return f'{result}'
